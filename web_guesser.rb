d0517b083ef6f3ac85905b242e88325715070a05
polycarpevergete <xavier.sassi@gmail.com>
2019-01-07 18:24:37 +0700

    I1 : generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+
+x=1
+y=2
+
+while x!= y do
+x = rand(101)
+y = rand(101)
+end
 
 get '/' do
     "hello, World!"
+    "THE SECRET NUMBER IS #{x} where #{y} is the secret number"
 end
