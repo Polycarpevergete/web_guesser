342d0b77df05e40cd55219d3203e349d781ba61c
polycarpevergete <xavier.sassi@gmail.com>
2019-01-08 12:08:50 +0700

    I4: Extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -10,6 +10,32 @@ x = rand(101)
 y = rand(101)
 end
 
+message = "fuck"
+
 get '/' do
-    erb :index, :locals => {:x => x}
+    params["guess"]
+    if params["guess"].to_i > x
+        message = "too High"
+        color = 'salmon'
+    end
+    if params["guess"].to_i < x
+        message = "too low"
+        color = 'salmon'
+    end
+    if params["guess"].to_i == x
+        message = "correct!"    
+        color = 'green'
+    end
+    if params["guess"].to_i > (x+5)
+        message = "way too high"
+        color = 'red'
+    end
+    if params["guess"].to_i < (x-5)
+        message = "way too low"
+        color = 'red'
+    end
+    
+    erb :index, :locals => {:x => x, :message => message, :color =>color}
 end
+
+
